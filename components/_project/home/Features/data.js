04b1b4d95e531864d58725ded61219f4c73e9b49
Ben Silverman <bsilverm@mit.edu>
2019-10-12 17:44:58 -0400

    Update gifs, video with new nomenclature

--- components/_project/home/Features/data.js
+++ components/_project/home/Features/data.js
@@ -4,7 +4,7 @@ export default [
 		header: 'Timeline',
 		label: `Understand the chronology of events, documents and the involvement of protagonists.`,
 		gif:
-			'https://res.cloudinary.com/podocu/video/upload/c_scale,w_1800,du_3,f_gif,so_0,vc_h264/e_loop/v1551439238/university/hidden_perspectives/hidden-perspectives-trailer.gif',
+			'`https://i.imgur.com/X5PXAjh.gif`',
 	},
 	{
 		key: 'featureconnections',
@@ -12,14 +12,14 @@ export default [
 		label:
 			'Investigate links within the large network of documents, events, and protagonists..',
 		gif:
-			'https://res.cloudinary.com/podocu/video/upload/c_scale,w_1800,du_3,f_gif,so_4,vc_h264/e_loop/v1551439238/university/hidden_perspectives/hidden-perspectives-trailer.gif',
+			'https://i.imgur.com/3ovhh4c.gif',
 	},
 	{
 		key: 'featureoriginaldocuments',
 		header: 'Original Documents',
 		label: 'Study scans and transcripts of all primary sources.',
 		gif:
-			'https://res.cloudinary.com/podocu/video/upload/c_scale,w_1800,du_3,f_gif,so_24,vc_h264/e_loop/v1551439238/university/hidden_perspectives/hidden-perspectives-trailer.gif',
+			'https://i.imgur.com/Cp4Ispm.gif',
 	},
 	{
 		key: 'featurecontext',
@@ -27,13 +27,13 @@ export default [
 		label:
 			'Explore each individual entry, its historical context, and relations to other contents.',
 		gif:
-			'https://res.cloudinary.com/podocu/video/upload/c_scale,w_1800,du_3,f_gif,so_16,vc_h264/e_loop/v1551439238/university/hidden_perspectives/hidden-perspectives-trailer.gif',
+			'https://i.imgur.com/EsROSog.gif',
 	},
 	{
 		key: 'featuresearch',
 		header: 'Search',
 		label: 'Find any entry easily using the text search.',
 		gif:
-			'https://res.cloudinary.com/podocu/video/upload/c_scale,w_1800,du_3,f_gif,so_38,vc_h264/e_loop/v1551439238/university/hidden_perspectives/hidden-perspectives-trailer.gif',
+			'https://i.imgur.com/nwZXrGF.gif',
 	},
 ]
