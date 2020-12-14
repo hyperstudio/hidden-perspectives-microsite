ba8b12658719d2818b45f83d18832fe949f2378b
Ben Silverman <bsilverm@mit.edu>
2020-12-14 13:38:00 -0700

    #5 Footer styling + accessibility link

--- src/components/_project/home/Footer/index.js
+++ src/components/_project/home/Footer/index.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { space } from 'styled-system';
 import OuterRow from '../../../Layout/OuterRow';
 import { media } from '../../../../lib/index';
+import Typography from '../../../Typography';
+import Link from '../../../Typography/Link';
 import Logo from './Logo';
 
 const Section = styled('div')`
@@ -17,38 +19,31 @@ const Wrapper = styled('div')`
 	width: 100%;
 	margin-top: 5rem;
 	display: flex;
-	justify-content: center;
+  justify-content: center;
 	${space}
 `;
 const OuterWrapper = styled(OuterRow)`
 	display: flex;
 	flex-direction: row;
-	> div {
-		width: 50%;
-	}
+  justify-content: space-between;
+  max-width: 100%;
+  width: 90%;
 	${media.xs`
 		flex-direction: column;
 		> div {
 			width: auto;
-			margin-bottom: 48px;
 		}
 	`}
 `;
 
 const LogoWrapper = styled('div')`
-	bottom: 0;
-	padding-left: 4px;
-	padding-bottom: 4px;
 	justify-self: flex-end;
-	position: absolute;
 	${media.xs`
 		display: none;
 	`}
 `;
 
 const MobileLogo = styled('div')`
-	padding-left: 4px;
-	padding-top: 120px;
 	justify-self: center;
 	display: none;
 	${media.xs`
@@ -56,18 +51,35 @@ const MobileLogo = styled('div')`
 	`}
 `;
 
+const FooterLink = styled(Link)`
+  &:focus {
+		box-shadow: inset 0 0 0 2px #c0c0c0;
+	}
+`;
+
+const FooterType = styled(Typography)`
+  margin-top: 0.5rem;
+  margin-bottom: 0;
+	&:focus {
+		box-shadow: inset 0 0 0 2px #c0c0c0;
+	}
+`;
+
 const Footer = () => (
-  <Wrapper py={[6, 6, 6]}>
+  <Wrapper py={[4, 4, 4]}>
     <OuterWrapper rowWidth="wide">
       <Section>
         <LogoWrapper>
           <Logo />
         </LogoWrapper>
-      </Section>
-      <Section>
         <MobileLogo>
           <Logo />
         </MobileLogo>
+      </Section>
+      <Section>
+        <FooterLink href="https://accessibility.mit.edu/">
+          <FooterType type="body2" color="typoPrimary">Accessibility</FooterType>
+        </FooterLink>
       </Section>
     </OuterWrapper>
   </Wrapper>
